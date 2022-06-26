1b2d3b843ccdc25670cd3a571925cc1a0aac1edd
Grigory <grishavinyar@icloud.com>
2022-06-26 21:39:30 +0300

    Added login function to network service

--- GBShop/GBShop/NetworkLayer/NetworkService.swift
+++ GBShop/GBShop/NetworkLayer/NetworkService.swift
@@ -13,6 +13,34 @@ protocol NetworkServiceProtocol {
 
 class NetworkService: NetworkServiceProtocol {
     
+    func logIn(urlString: String, name: String, password: String, result: @escaping (Bool) -> Void) {
+        
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration)
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        urlComponents?.queryItems = [
+            URLQueryItem(name: "userName", value: name),
+            URLQueryItem(name: "password", value: password)
+        ]
+        
+        guard let urlFromComponents = urlComponents?.url else { return }
+        var request = URLRequest(url: urlFromComponents)
+        
+        request.httpMethod = "POST"
+        
+        session.dataTask(with: request) { data, response, error in
+            guard data != nil, error == nil else {
+                result(false)
+                return
+            }
+            result(true)
+        }
+        
+    }
+    
     func fetchData<T: Decodable>(urlString: String, completion: @escaping (T) -> Void) {
         
         let configuration = URLSessionConfiguration.default
